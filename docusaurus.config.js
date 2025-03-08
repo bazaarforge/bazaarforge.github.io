2ddf3729ed4e5a763a9f61cb54fa586f4190c28f
gsouf <sghzal@gmail.com>
2025-03-08 11:36:31 +0100

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,73 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'BazaarForge',
+  tagline: 'Elevating Your Shopify Experience',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+
+          sidebarPath: './sidebars.js'
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'BazaarForge',
+        logo: {
+          alt: 'BazaarForge Logo',
+          src: 'img/smp-logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'smartCollectionProSidebar',
+            position: 'left',
+            label: 'Smart collection pro',
+          },
+        ],
+      }
+    }),
+};
+
+export default config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,60 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
+
+const FeatureList = [
+  {
+    title: 'Smart Collection Pro',
+    subtitle: "Supercharged Smart Collections",
+    SvgSrc: "/img/smp-logo.svg",
+    description: (
+      <>
+        Take Shopify collections to the next level with advanced conditions, auto-syncing and hierarchy.
+      </>
+    ),
+  },
+];
+
+function Feature({SvgSrc, title, subtitle, description}) {
+  return (
+    <div className={clsx('col col--8 col--offset-2')}>
+      <div className={styles.feature}>
+        <div className="text--center">
+          <img src={SvgSrc} className={styles.featureSvg} role="img"/>
+        </div>
+        <div className="padding-horiz--md">
+          <Heading as="h3">{title}</Heading>
+          <Heading as="h4">{subtitle}</Heading>
+          <p>{description}</p>
+          <div>
+            <Link className="button button--secondary" href="/docs/smart-collection-pro/intro">View documentation</Link>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <div>
+    <div>
+        <Heading as="h2" className="text--center">
+          Our featured app
+        </Heading>
+      </div>
+      <section className={styles.features}>
+
+        <div className="container">
+          <div className="row justify-content-center">
+            {FeatureList.map((props, idx) => (
+              <Feature key={idx} {...props} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
